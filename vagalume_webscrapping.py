efb9d51ca08c922a9758f3c68005a845a67eb46a
astromar2187 <maria.ferreira@ufpi.edu.br>
2025-04-28 01:39:07 -0300

    fix a few things

--- vagalume_webscrapping.py
+++ vagalume_webscrapping.py
@@ -6,7 +6,7 @@ import re
 artistas = [linha.strip() for linha in open('novo_artistas.txt', encoding='utf-8')]
 
 # Monta a url
-url = 'https://www.vagalume.com.br/' + artistas[1] + "/discografia/"
+url = 'https://www.vagalume.com.br/' + artistas[-1] + "/discografia/"
 
 # Faz a requisição
 req = requests.get(url)
@@ -14,16 +14,19 @@ req = requests.get(url)
 # puxa o html -> usa req.content e não req.text por erros de encoding detectados em testes
 soup = bs4.BeautifulSoup(req.content, "html.parser")
 
-
 #nome do artista
-artist_name = soup.find("h1", "darkBG long").get_text().strip()
+artist_name = soup.find("h1", "darkBG long")
+if artist_name == None:
+    artist_name = soup.find("h1", "darkBG")
+artist_name = artist_name.get_text().strip()
+
 
 # tags associadas ao artista
 artist_tags_list = soup.find("ul", "subHeaderTags h14")
 artist_tags = []
 
 if artist_tags_list:
-    tag_items = artist_tags_list.find_all("li")
+    tag_items = artist_tags_list.find_all("a")
     for tag in tag_items:
         tag_text = tag.get_text().strip()
         artist_tags.append(tag_text)
@@ -37,6 +40,9 @@ albuns = soup.find_all("div", "topLetrasWrapper")
 
 for album_div in albuns:
     album = {}
+
+    album["artist_name"] = artist_name
+    album["artist_tags"] = artist_tags
 
     album_title_elem = album_div.find("h1", "albumTitle")
     if album_title_elem:
